a851004dee1c2d8fe910fc598c22ef71eb74d1b3
SamWaku <samuelwaku1st@gmail.com>
2025-02-13 00:12:35 +0200

    Modelsstring

--- Models/WalletTransaction.cs
+++ Models/WalletTransaction.cs
@@ -2,6 +2,9 @@ namespace api.Models
 {
     public class WalletTransaction
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }  = string.Empty;
+        public Wallet? Wallet { get; set; } //Navigation property
+        public int MyProperty { get; set; }
     }
 }
