39ad2d65f7cc3f16d26303c50e40abf7b8bb8a72
SamWaku <samuelwaku1st@gmail.com>
2025-02-16 12:52:06 +0200

    getbyid'

--- Controllers/WalletController.cs
+++ Controllers/WalletController.cs
@@ -23,5 +23,11 @@ namespace api.Controllers
             var wallets = _context.Wallets.ToList(); //defered execution... sql completes the de
             return Ok(wallets);
         }
+
+        [HttpGet({"id"})]
+        public IActionResult GetById([FromRoute] int id)
+        {
+
+        }
     }
 }
