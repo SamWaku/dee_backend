982d5d9c94c5320605897e79a606ebbe61404e6d
SamWaku <samuelwaku1st@gmail.com>
2025-02-16 17:46:18 +0200

    refactored

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc495a3cc1fb714615b437022edcb4ce9deb12e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e8d9dd024317f30872bf8698be4600041e4ded5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
