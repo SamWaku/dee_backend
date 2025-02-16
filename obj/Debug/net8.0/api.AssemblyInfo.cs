2343a8e4fc4b55b36187c86e00005dc31ee87a35
SamWaku <samuelwaku1st@gmail.com>
2025-02-16 21:32:36 +0200

    userid

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982d5d9c94c5320605897e79a606ebbe61404e6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669ab8d7c73b124592259ea21285abc373201bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
